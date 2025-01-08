5aa7b5e56d69208496a85eb488486eed4e61f270
shouta0715 <kshouta0715@gmail.com>
2025-01-08 19:39:04 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by k21047kk on 2025/01/08.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
